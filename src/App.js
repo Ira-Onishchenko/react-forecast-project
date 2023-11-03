e48a926c86973ead18ccef42521a82edec6d3689
Ira Onishchenko <Irena3@ukr.net>
2023-11-03 14:56:20 +0200

    Added bootstrap and updated react

--- src/App.js
+++ src/App.js
@@ -3,16 +3,15 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>React Weather App</h1>
-      <footer>
-        This project was coded by Ira Onishchenko and is {""}
-        <a
-          href="https://github.com/Ira-Onishchenko/portfolio-project"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>React Weather App</h1>
+        <footer>
+          This project was coded by Ira Onishchenko and is {""}
+          <a href="https://github.com/Ira-Onishchenko/portfolio-project">
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
