f1db40ec92fe6d12c1caa2a075c620bb30cfea76
Ira Onishchenko <Irena3@ukr.net>
2023-11-03 14:24:03 +0200

    Added GitHub link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">React Weather App</div>;
+  return (
+    <div className="App">
+      <h1>React Weather App</h1>
+      <footer>
+        This project was coded by Ira Onishchenko and is {""}
+        <a
+          href="https://github.com/Ira-Onishchenko/portfolio-project"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
