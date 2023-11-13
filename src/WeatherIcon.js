cf66cf00379bfe18e2915893bede49f99dcd8d56
Ira Onishchenko <Irena3@ukr.net>
2023-11-13 13:19:26 +0200

    Updated css

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -26,7 +26,7 @@ export default function WeatherIcon(props) {
   return (
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
-      color="silver"
+      color="grey"
       size={props.size}
       animate={true}
     />
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -19,8 +19,8 @@ export default function WeatherInfo(props) {
         </div>
         <div className="col-6">
           <ul>
-            <li>Humidity:{props.data.humidity}%</li>
-            <li>Wind:{props.data.wind}km/h</li>
+            <li className="humidity">Humidity: {props.data.humidity}%</li>
+            <li className="wind">Wind: {props.data.wind}km/h</li>
           </ul>
         </div>
       </div>
